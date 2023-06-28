aa8a3f18dce9410e76c6c06aecbb83f4a736d669
rahuljsaliaan <rahuljsaliaan@gmail.com>
2023-06-29 00:11:55 +0530

    added new welcome message feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -125,6 +125,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 // subscriber function
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -134,6 +138,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 
 init();
